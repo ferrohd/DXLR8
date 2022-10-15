74c62d2283a142bcae36bec424be4ff88711c8f8
Alessandro <aleferrara1998@gmail.com>
2022-10-15 11:26:46 +0200

    refactor funzion mases to snake case

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@ const uint16_t BLINKING_WINDOW = 4500; // 4.5 seconds
 const float BLINKING_INTERVAL = 0.25; // 0.25s blinking
 const float ACCELERATION_THRESHOLD = 0.5 * 9.81; // m/s^2
 
-enum breaking {
+enum breaking_state {
     NORMAL_BRAKING,
     HARD_BRAKING,
 };
 
-breaking state;
+breaking_state state;
 uint16_t hard_braking_start;
 
 void setup() {
@@ -21,21 +21,21 @@ void setup() {
 }
 
 // Define helper functions
-float readAcceleration();
-void turnOnBrakeLight();
-void toggleBrakeLight();
-void turnOffBrakeLight();
+float read_acceleration();
+void turn_on_brake_light();
+void turn_off_brake_light();
+void toggle_brake_light();
 
 // If the acceleration is greater than 1.5g, then we are braking hard
 // If we are breaking hard, make the LED blink at 4Hz for 4.5 seconds then go back to normal
 void loop() {
-    float acceleration = readAcceleration();
+    float acceleration = read_acceleration();
     Serial.println(acceleration);
     switch (state) {
         
         case NORMAL_BRAKING:
             Serial.println("Normal braking");
-            turnOnBrakeLight();
+            turn_on_brake_light();
             if (acceleration < ACCELERATION_THRESHOLD) break; // or return
             
             state = HARD_BRAKING;
@@ -53,27 +53,27 @@ void loop() {
             // Blinking
             if (millis() % int (BLINKING_INTERVAL * 1000) < BLINKING_INTERVAL * 500) {
                 // Turn on LED
-                turnOnBrakeLight();
+                turn_on_brake_light();
             } else {
                 // Turn off LED
-                turnOffBrakeLight();
+                turn_off_brake_light();
             }
             break;
     }
 }
 
-float readAcceleration() {
-    float x = random(1, 5);
-    float y = random(1, 5);
-    float z = random(1, 5);
+float read_acceleration() {
+    float x = random(1, 6);
+    float y = random(1, 6);
+    float z = random(1, 6);
     float vec = sqrt(x * x + y * y + z * z);
     return x;
 }
 
-void turnOnBrakeLight() {
+void turn_on_brake_light() {
     Serial.println("ON");
 }
-void turnOffBrakeLight() {
+void turn_off_brake_light() {
     Serial.println("OFF");
 }
-void toggleBrakeLight() {}
+void toggle_brake_light() {}
